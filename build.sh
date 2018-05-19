1d8dbf6a7643f58737c765cf1c18178380b2c23a
Mike Sinkovsky <msink@permonline.ru>
2018-05-19 17:48:56 +0500

    fixed build for Linux

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-mkdir -p build/libui
-pushd build/libui
-cmake -G "Unix Makefiles" -DBUILD_SHARED_LIBS=OFF ../../libui
-make tester examples
-popd
+if [[ ! -f build/libui/out/libui.a ]]; then
+  git submodule update --init
+  mkdir -p build/libui
+  pushd build/libui
+  cmake -G "Unix Makefiles" -DBUILD_SHARED_LIBS=OFF ../../libui
+  make -j 4 tester examples
+  popd
+fi
 
 ./gradlew build
