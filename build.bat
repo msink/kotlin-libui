31c963fe67ffe97ca326c844731dd1c81e633415
Mike Sinkovsky <msink@permonline.ru>
2018-05-23 18:16:16 +0500

    better handling for windows resource compiler

--- build.bat
+++ build.bat
@@ -1,8 +1,7 @@
 @echo off
 
-set "PATH=C:\msys64\mingw64\bin;%PATH%"
-
 if not exist build\libui\out\libui.a (
+  set "PATH=C:\msys64\mingw64\bin;%PATH%"
   git submodule update --init
   md build\libui
   cd build\libui
