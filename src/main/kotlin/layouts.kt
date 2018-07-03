d5f21f3139618dc610ec29b26b6ebe8ccfaf7538
Mike Sinkovsky <msink@permonline.ru>
2018-07-03 12:59:17 +0500

    cleanup

--- src/main/kotlin/layouts.kt
+++ src/main/kotlin/layouts.kt
@@ -65,12 +65,11 @@ fun Box.delete(index: Int) = uiBoxDelete(ptr, index)
 /** A container that organize children as labeled fields. */
 class Form : Control<uiForm>(uiNewForm()) {
 
+    /** adapter for DSL builders */
     inner class Field(val label: String, val stretchy: Boolean = false) : Container {
         val form: Form get() = this@Form
-
-        /** Adds the given widget to the end of the form. */
         override fun <T : Control<*>> add(widget: T): T {
-            uiFormAppend(ptr, label, widget.ctl, if (stretchy) 1 else 0)
+            form.add(label, widget, stretchy)
             return widget
         }
     }
@@ -81,6 +80,10 @@ var Form.padded: Boolean
     get() = uiFormPadded(ptr) != 0
     set(padded) = uiFormSetPadded(ptr, if (padded) 1 else 0)
 
+/** Adds the given widget to the end of the form. */
+fun Form.add(label: String, widget: Control<*>, stretchy: Boolean = false) =
+    uiFormAppend(ptr, label, widget.ctl, if (stretchy) 1 else 0)
+
 /** deletes the nth control of the form. */
 fun Form.delete(index: Int) = uiFormDelete(ptr, index)
 
@@ -89,25 +92,31 @@ fun Form.delete(index: Int) = uiFormDelete(ptr, index)
 /** A container that show each chidren in a separate tab. */
 class TabPane : Control<uiTab>(uiNewTab()) {
 
+    /** adapter for DSL builders */
     inner class Page(val label: String) : Container {
         val pane: TabPane get() = this@TabPane
         val page = pane.numPages // last page
-
-        /** Set the child widget of the Page. */
         override fun <T : Control<*>> add(widget: T): T {
-            uiTabAppend(pane.ptr, label, widget.ctl)
+            pane.add(label, widget)
             return widget
         }
+        var margined: Boolean
+            get() = pane.getMargined(page)
+            set(margined) = pane.setMargined(page, margined)
     }
 }
 
-/** Whether page of the TabPane has margins around its child. */
-var TabPane.Page.margined: Boolean
-    get() = uiTabMargined(pane.ptr, page) != 0
-    set(margined) = uiTabSetMargined(pane.ptr, page, if (margined) 1 else 0)
+/** Whether page n (starting at 0) of the Tab has margins around its child. */
+fun TabPane.getMargined(page: Int): Boolean = uiTabMargined(ptr, page) != 0
+fun TabPane.setMargined(page: Int, margined: Boolean) = uiTabSetMargined(ptr, page, if (margined) 1 else 0)
 
 /** Number of pages in the TabPane. */
 val TabPane.numPages: Int get() = uiTabNumPages(ptr)
+
+/** Adds the given page to the end of the TabPane. */
+fun TabPane.add(label: String, widget: Control<*>) {
+    uiTabAppend(ptr, label, widget.ctl)
+}
 
 /** Adds the given page to the TabPane such that it is the nth page of the TabPane (starting at 0). */
 fun TabPane.insert(page: Int, name: String, widget: Control<*>) = uiTabInsertAt(ptr, name, page, widget.ctl)
@@ -120,17 +129,7 @@ fun TabPane.delete(page: Int) = uiTabDelete(ptr, page)
 /** A powerful container that allow to specify size and position of each children. */
 class GridPane : Control<uiGrid>(uiNewGrid()) {
 
-    /** Adds new Control to the end of the GridPane.
-     *
-     *  @param[x] The x-coordinate of the Control.
-     *  @param[y] The y-coordinate of the Control.
-     *  @param[xspan] The width of the Control.
-     *  @param[yspan] The height of the Control.
-     *  @param[hexpand] The horizontal expand of Control.
-     *  @param[halign] The horizontal alignment of Control.
-     *  @param[vexpand] The vertical expand of Control.
-     *  @param[valign] The vertical alignment of Control.
-     */
+    /** adapter for DSL builders */
     inner class Cell(
         val x: Int = 0,
         val y: Int = 0,
@@ -142,13 +141,8 @@ class GridPane : Control<uiGrid>(uiNewGrid()) {
         val valign: uiAlign = uiAlignFill
     ) : Container {
         val pane: GridPane get() = this@GridPane
-
-        /** Set the child widget of the Cell. */
         override fun <T : Control<*>> add(widget: T): T {
-            uiGridAppend(pane.ptr, widget.ctl,
-                x, y, xspan, yspan,
-                if (hexpand) 1 else 0, halign,
-                if (vexpand) 1 else 0, valign)
+            pane.add(widget, x, y, xspan, yspan, hexpand, halign, vexpand, valign)
             return widget
         }
     }
@@ -158,6 +152,33 @@ class GridPane : Control<uiGrid>(uiNewGrid()) {
 var GridPane.padded: Boolean
     get() = uiGridPadded(ptr) != 0
     set(padded) = uiGridSetPadded(ptr, if (padded) 1 else 0)
+
+/** Adds the given Control to the end of the GridPane.
+ *
+ *  @param[widget] The Control to be added.
+ *  @param[x] The x-coordinate of the Control's location.
+ *  @param[y] The y-coordinate of the Control's location.
+ *  @param[xspan] The width of the Control.
+ *  @param[yspan] The height of the Control.
+ *  @param[hexpand] The horizontal expand of Control.
+ *  @param[halign] The horizontal alignment of Control.
+ *  @param[vexpand] The vertical expand of Control.
+ *  @param[valign] The vertical alignment of Control.
+ */
+fun GridPane.add(
+    widget: Control<*>,
+    x: Int = 0,
+    y: Int = 0,
+    xspan: Int = 1,
+    yspan: Int = 1,
+    hexpand: Boolean = false,
+    halign: uiAlign = uiAlignFill,
+    vexpand: Boolean = false,
+    valign: uiAlign = uiAlignFill
+) = uiGridAppend(ptr, widget.ctl,
+        x, y, xspan, yspan,
+        if (hexpand) 1 else 0, halign,
+        if (vexpand) 1 else 0, valign)
 
 /** Insert the given Control after existing Control.
  *
@@ -171,8 +192,8 @@ var GridPane.padded: Boolean
  *  @param[vexpand] The vertical expand of Control.
  *  @param[valign] The vertical alignment of Control.
  */
-fun <T : Control<*>> GridPane.insert(
-    widget: T,
+fun GridPane.insert(
+    widget: Control<*>,
     existing: Control<*>,
     at: uiAt,
     xspan: Int = 1,
@@ -181,10 +202,9 @@ fun <T : Control<*>> GridPane.insert(
     halign: uiAlign = uiAlignFill,
     vexpand: Boolean = false,
     valign: uiAlign = uiAlignFill
-): T {
+) {
     uiGridInsertAt(ptr, widget.ctl, existing.ctl,
-    at, xspan, yspan,
-    if (hexpand) 1 else 0, halign,
-    if (vexpand) 1 else 0, valign)
-    return widget
+        at, xspan, yspan,
+        if (hexpand) 1 else 0, halign,
+        if (vexpand) 1 else 0, valign)
 }
