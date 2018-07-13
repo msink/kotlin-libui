f7a36580ec936602a9225999064e3fdecff9e2b6
Mike Sinkovsky <msink@permonline.ru>
2018-07-13 16:49:24 +0500

    split build.gradle

--- build.bat
+++ build.bat
@@ -1,13 +1,13 @@
 @echo off
 
-if not exist build\libui\out\libui.a (
+if not exist libui-ktx\build\libui\out\libui.a (
   set "PATH=C:\msys64\mingw64\bin;%PATH%"
   git submodule update --init
-  md build\libui
-  cd build\libui
-  cmake -G "MinGW Makefiles" -DBUILD_SHARED_LIBS=OFF ..\..\libui
+  md libui-ktx\build\libui
+  cd libui-ktx\build\libui
+  cmake -G "MinGW Makefiles" -DBUILD_SHARED_LIBS=OFF ..\..\..\libui
   mingw32-make -j 4 tester examples
-  cd ..\..
+  cd ..\..\..
 )
 
 gradlew build
--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [[ ! -f build/libui/out/libui.a ]]; then
+if [[ ! -f libui-ktx/build/libui/out/libui.a ]]; then
   git submodule update --init
-  mkdir -p build/libui
-  pushd build/libui
-  cmake -G "Unix Makefiles" -DBUILD_SHARED_LIBS=OFF ../../libui
+  mkdir -p libui-ktx/build/libui
+  pushd libui-ktx/build/libui
+  cmake -G "Unix Makefiles" -DBUILD_SHARED_LIBS=OFF ../../../libui
   make -j 4 tester examples
   popd
 fi
