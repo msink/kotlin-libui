389998a5b2525fb983538fcf1dc6b8dcba843f35
Mike Sinkovsky <msink@permonline.ru>
2018-07-16 12:35:01 +0500

    move libui source to libui/src

--- build.bat
+++ build.bat
@@ -1,13 +1,13 @@
 @echo off
 
-if not exist libui-ktx\build\libui\out\libui.a (
+if not exist libui\build\out\libui.a (
   set "PATH=C:\msys64\mingw64\bin;%PATH%"
   git submodule update --init
-  md libui-ktx\build\libui
-  cd libui-ktx\build\libui
-  cmake -G "MinGW Makefiles" -DBUILD_SHARED_LIBS=OFF ..\..\..\libui
+  md libui\build
+  cd libui\build
+  cmake -G "MinGW Makefiles" -DBUILD_SHARED_LIBS=OFF ..\src
   mingw32-make -j 4 tester examples
-  cd ..\..\..
+  cd ..\..
 )
 
 gradlew build
