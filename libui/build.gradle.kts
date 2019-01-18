fd2f9cd8e9b76414f96b4e9762b646b16b346245
Mike Sinkovsky <msink@permonline.ru>
2019-01-18 13:02:12 +0500

    Migrate publishing to Kotlin DSL

--- libui/build.gradle.kts
+++ libui/build.gradle.kts
@@ -2,6 +2,10 @@
 
 @file:Suppress("SpellCheckingInspection")
 
+import com.jfrog.bintray.gradle.BintrayExtension
+import com.jfrog.bintray.gradle.BintrayUploadTask
+import java.util.Date
+
 plugins {
     id("kotlin-multiplatform")
     id("de.undercouch.download")
@@ -78,6 +82,72 @@ tasks.withType(org.jetbrains.kotlin.gradle.tasks.CInteropProcess::class).all {
     dependsOn(unpackArchive)
 }
 
-apply {
-    from("publish.gradle")
+/*
+ * Publishing
+ */
+val POM_SCM_URL: String by project
+val POM_SCM_CONNECTION: String by project
+val POM_SCM_DEV_CONNECTION: String by project
+val BINTRAY_USER: String by project
+val BINTRAY_REPO: String by project
+
+val publications = project.publishing.publications.withType(MavenPublication::class.java).map {
+    with(it.pom) {
+        withXml {
+            val root = asNode()
+            root.appendNode("name", "libui")
+            root.appendNode("description", "Kotlin/Native interop to libui: a portable GUI library")
+            root.appendNode("url", POM_SCM_URL)
+        }
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set(POM_SCM_URL)
+                distribution.set("repo")
+            }
+        }
+        developers {
+            developer {
+                id.set("msink")
+                name.set("Mike Sinkovsky")
+                email.set("msink@permonline.ru")
+            }
+        }
+        scm {
+            url.set(POM_SCM_URL)
+            connection.set(POM_SCM_CONNECTION)
+            developerConnection.set(POM_SCM_DEV_CONNECTION)
+        }
+    }
+    println("Publishing artifact '${it.groupId}:${it.artifactId}:${it.version}' from publication '${it.name}'")
+    it
+}
+
+publishing {
+    repositories {
+        maven { url = uri("https://bintray.com/$BINTRAY_USER/$BINTRAY_REPO") }
+    }
+}
+
+tasks.withType<BintrayUploadTask>().configureEach {
+    dependsOn("publishToMavenLocal")
+    setPublications(publications)
+}
+
+bintray {
+    user = BINTRAY_USER
+    key = System.getenv("BINTRAY_API_KEY")
+    override = true // for multi-platform Kotlin/Native publishing
+    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+        userOrg = BINTRAY_USER
+        repo = BINTRAY_REPO
+        name = "libui"
+        setLicenses("MIT")
+        vcsUrl = POM_SCM_URL
+        version(delegateClosureOf<BintrayExtension.VersionConfig> {
+            name = project.version.toString()
+            vcsTag = project.version.toString()
+            released = Date().toString()
+        })
+    })
 }
