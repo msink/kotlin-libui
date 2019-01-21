9ca0099a632f44d7aa7461657a6a7f46730b3e6b
Mike Sinkovsky <msink@permonline.ru>
2019-01-21 15:50:56 +0500

    Added Apache-2.0 license

--- libui/build.gradle.kts
+++ libui/build.gradle.kts
@@ -95,6 +95,10 @@ val publications = project.publishing.publications.withType(MavenPublication::cl
                 url.set(Publish.pom.url)
                 distribution.set("repo")
             }
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
         }
         developers {
             developer {
