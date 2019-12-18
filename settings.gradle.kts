7bb79d53d33aff70fe2102b71b28344e0c6b7108
Mike Sinkovsky <msink@permonline.ru>
2019-12-18 17:32:24 +0500

    Test on Github Workflow and enable Gradle BuildScan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,17 @@ pluginManagement {
     }
 }
 
+plugins {
+  id("com.gradle.enterprise").version("3.1.1")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
+
 include(":libui")
 
 include(":samples:controlgallery")
