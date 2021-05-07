ad7cdc04e0fbcbdb36b29f0d398385228f4af60b
Mike Sinkovsky <msink@permonline.ru>
2021-05-07 18:00:41 +0500

    hack: skip "libui/" in Dokka path

--- tools/dokka-mygfm/src/main/kotlin/location/MarkdownLocationProvider.kt
+++ tools/dokka-mygfm/src/main/kotlin/location/MarkdownLocationProvider.kt
@@ -2,7 +2,7 @@ package org.jetbrains.dokka.gfm.location
 
 import org.jetbrains.dokka.base.resolvers.local.DokkaLocationProvider
 import org.jetbrains.dokka.base.resolvers.local.LocationProviderFactory
-import org.jetbrains.dokka.pages.RootPageNode
+import org.jetbrains.dokka.pages.*
 import org.jetbrains.dokka.plugability.DokkaContext
 
 class MarkdownLocationProvider(
@@ -16,4 +16,17 @@ class MarkdownLocationProvider(
     }
 
     override val PAGE_WITH_CHILDREN_SUFFIX = "README"
+
+    override fun pathTo(node: PageNode, context: PageNode?): String {
+        return super.pathTo(node, context).removePrefix("libui/")
+    }
+
+    override fun ancestors(node: PageNode): List<PageNode> {
+        return generateSequence(node) { it.parent() }
+            .filterNot { it is RendererSpecificPage }
+            .filterNot { it is ModulePageNode }
+            .toList()
+    }
+
+    private fun PageNode.parent() = pageGraphRoot.parentMap[this]
 }
