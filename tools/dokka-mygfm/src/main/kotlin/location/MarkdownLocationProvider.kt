2a31206d81cb3a636845673e3a5e29e4105d8c69
Mike Sinkovsky <msink@permonline.ru>
2021-05-22 18:23:37 +0500

    hack: remove links to self issue: https://github.com/Kotlin/dokka/issues/1582

--- tools/dokka-mygfm/src/main/kotlin/location/MarkdownLocationProvider.kt
+++ tools/dokka-mygfm/src/main/kotlin/location/MarkdownLocationProvider.kt
@@ -2,6 +2,8 @@ package org.jetbrains.dokka.gfm.location
 
 import org.jetbrains.dokka.base.resolvers.local.DokkaLocationProvider
 import org.jetbrains.dokka.base.resolvers.local.LocationProviderFactory
+import org.jetbrains.dokka.links.DRI
+import org.jetbrains.dokka.model.DisplaySourceSet
 import org.jetbrains.dokka.pages.*
 import org.jetbrains.dokka.plugability.DokkaContext
 
@@ -19,6 +21,17 @@ class MarkdownLocationProvider(
 
     override fun pathTo(node: PageNode, context: PageNode?): String {
         return super.pathTo(node, context).removePrefix("libui/")
+    }
+
+    override fun resolve(dri: DRI, sourceSets: Set<DisplaySourceSet>, context: PageNode?): String? {
+        val it = super.resolve(dri, sourceSets, context)
+        if ((context is ClasslikePageNode && it == PAGE_WITH_CHILDREN_SUFFIX + extension) ||
+            (context is MemberPageNode && it == identifierToFilename(context.name) + extension)
+        ) {
+            return null
+        } else {
+            return it
+        }
     }
 
     override fun ancestors(node: PageNode): List<PageNode> {
